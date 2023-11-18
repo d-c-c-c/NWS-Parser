e40e2244e46d5c605cca4b7ce7245a0314f04fb6
d-c-c-c <djc3mu@virginia.edu>
2023-11-17 21:06:47 -0500

    created forecast.js

---
+++ weather-display/src/components/forecast.js
@@ -0,0 +1,14 @@
+import "../styles/forecast.css"
+import React, { useState, useEffect } from "react";
+
+const Forecast = () => {
+
+
+    return (
+        <div>
+            <p>Hello</p>
+        </div>
+    )
+}
+
+export default Forecast

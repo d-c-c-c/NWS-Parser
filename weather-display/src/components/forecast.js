952ccdc27b2c4f34c682f722b0db8f6e6501c545
d-c-c-c <djc3mu@virginia.edu>
2023-11-17 22:19:33 -0500

    setting up forecast display

--- weather-display/src/components/forecast.js
+++ weather-display/src/components/forecast.js
@@ -3,10 +3,20 @@ import React, { useState, useEffect } from "react";
 
 const Forecast = () => {
 
-
+    //List of days used for the forecast list
+    const days = ["Monday","Tuesday","Wednesday","Thursday","Friday"];
     return (
-        <div>
-            <p>Hello</p>
+
+        <div className = "forecastContainer">
+
+            {days.map((day, index) => (
+
+                <div className = "forecast-item-container">
+                    <li key = {index}>{day}</li>
+                    <img alt = "weather" className = "forecast-weather-icon" src={`icons/unknown.png`} />
+                    <label>Test</label>
+                </div>
+            ))}
         </div>
     )
 }
